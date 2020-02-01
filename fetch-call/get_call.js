9420e0ec9771ef2aea62495b4104779a477e2346
SatanLabs , Inc <46850618+satanlabs@users.noreply.github.com>
2020-02-01 21:03:20 +0530

    Create get_call.js

---
+++ fetch-call/get_call.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const fetch = require("node-fetch");
+
+fetch("http://www.example.com/some_api/data",
+    {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => {
+        if (res.status != 200) { throw new Error("HTTP-RESPONSE-STATUS NOT 200"); }
+        else { return res.json(); }
+    }
+    )
+    .then(
+        (result) => {
+            console.log("FETCH : result :", result);
+        },
+        (error) => {
+            console.log("FETCH : error :", error);
+        }
+    );
