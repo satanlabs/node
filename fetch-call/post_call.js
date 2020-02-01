04bca5563e3f1ec4fa6f63d72395b8e22aa0ca0e
SatanLabs , Inc <46850618+satanlabs@users.noreply.github.com>
2020-02-01 21:07:45 +0530

    Create post_call.js

---
+++ fetch-call/post_call.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const fetch = require("node-fetch");
+
+const requestPayload = {
+    name: "John Doe",
+    age: 27
+}
+
+fetch("http://example.com/api/users",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(requestPayload)
+    })
+    .then(res => {
+        if (res.status != 201) { throw new Error("HTTP-RESPONSE-STATUS NOT 201"); }
+        else { return res.json(); }
+    }
+    )
+    .then(
+        (result) => {
+            console.log("FETCH : result :", result);
+        },
+        (error) => {
+            console.log("FETCH : error :", error);
+        }
+    );
